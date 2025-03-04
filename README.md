d1dc68f8f9c3f21e3621bb1d87e6ad91dd3f3e02
lucy wonsower <lucywonsower@lucys-MacBook-Pro.local>
2025-03-04 13:01:23 -0800

    Update README and make some changes

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-# A template for a simnple react app and node server
+# A template for a simple react app and node server
 
 ### Instructions
-tbd
+1. Clone repo
+```
+git clone git@github.com:lwonsower/bootstrapped-project.git
+```
+
+2. Install dependencies
+```
+npm install
+```
+
+3. Start the app
+```
+npm run start
+```
+
+4. Make a bundle
+```
+npm run build
+```
+
+5. Start Server
+```
+npm start 
+```
+
--- server/index.js
+++ server/index.js
@@ -2,12 +2,26 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 8080;
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/api', (req, res) => {
-    res.json({ message: 'Hello from server!' });
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+
+app.get("/a-new-route", (req, res) => {
+    res.json({ message: "New return message!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(port, () => {
